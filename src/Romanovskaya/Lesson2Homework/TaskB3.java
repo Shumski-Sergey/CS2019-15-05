6001b9eb4604399b2ffc1abdc5be7e540caa81ab
Nastyaromanovsk <nastyaromanovskaya55@gmail.com>
2019-05-19 01:05:22 +0300

    lesson2TaskB1

---
+++ src/Romanovskaya/Lesson2Homework/TaskB3.java
@@ -0,0 +1,10 @@
+package Romanovskaya.Lesson2Homework;
+
+public class TaskB3 {
+
+    public static void main(String [] args)
+    {
+        String s = " С ";
+        System.out.print(s + "Новым Годом!");
+    }
+}
