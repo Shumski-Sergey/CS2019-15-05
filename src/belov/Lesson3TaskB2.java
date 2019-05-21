9ad0fcfab7c2d589f8c4fdc05b2ec9e989255702
dmbelov <dmbelov777@gmail.com>
2019-05-21 18:49:58 +0300

    Done Lesson 3 Tasks B,C

--- src/belov/Lesson3TaskB2.java
+++ src/belov/Lesson3TaskB2.java
@@ -3,5 +3,17 @@ package belov;
 public class Lesson3TaskB2 {
     public static void main(String[] args) {
 
+        long b, o, h;
+        float d, f;
+        b = o = h = 111;
+        d = f = (float) 111.111;
+
+        d = (int) d;
+        b = (byte) 0b111;
+        o = 0111;
+        h = 0x111;
+        f = (float) 111.111;
+
+        System.out.print(d+" "+b+" "+o+" "+h+" "+f);
     }
 }
