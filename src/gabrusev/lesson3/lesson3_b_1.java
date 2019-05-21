3af19f5e3b55f4438eb17e5ca56c931b646c81f6
pin2piva <happyowner88@gmail.com>
2019-05-21 23:56:11 +0300

    Не понял задание с литералами и чёт приуныл

--- src/gabrusev/lesson3/lesson3_b_1.java
+++ src/gabrusev/lesson3/lesson3_b_1.java
@@ -13,6 +13,29 @@ public class lesson3_b_1<dis> {
     }
 
     private static void dis(double a, double b, double c) {
-
+        double d;
+        d = b * b - 4 * a * c ;
+        abc (a,b,d);
     }
+    private static void abc(double a, double b, double d) {
+        if (d > 0) {
+            tD (a,b,d);
+        } else if (d == 0) {
+            oD (a,b,d) ;
+            } if (d < 0) {
+         System.out.println("Уравнение не имеет корней");
+            }
+    }
+    private static void tD(double a, double b, double d) {
+        double x1, x2;
+        x1 = ((-1) * b + Math.sqrt(d)) / 2 * a ;
+        x2 = ((-1) * b - Math.sqrt(d)) / 2 * a ;
+        System.out.println("x1 = " + x1 + "\n" + "x2 = " + x2);
+    }
+    private static void oD(double a, double b, double d) {
+        double x;
+        x = ((-1) * b + Math.sqrt(d)) / 2 * a ;
+        System.out.println("x = " + x);
+    }
+
 }
