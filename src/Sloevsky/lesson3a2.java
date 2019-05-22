a8419bc901bb6561997fbe9a33d18d7841a4fff6
Администратор <k.sloevsky@gmail.com>
2019-05-22 17:35:10 +0300

    lesson2-3

---
+++ src/Sloevsky/lesson3a2.java
@@ -0,0 +1,15 @@
+package Sloevsky;
+
+import java.util.Scanner;
+
+public class lesson3a2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\t\tМоё любимое стихотворение:");
+        String i = "\n У лукоморья дуб зелёный";
+        String j = "\nЗлатая цепь на дубе том";
+        String k = "\nИ днём и ночью кот учёный";
+        String l = "\nВсё ходит по цепи кругом";
+        System.out.println(i + j + k + l);
+    }
+}
