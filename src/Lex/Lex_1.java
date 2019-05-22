b20b94270d5224cb1a4d4112e15978020c205daa
Lex <Lex_8794@open.by>
2019-05-22 21:28:52 +0300

    My first task

---
+++ src/Lex/Lex_1.java
@@ -0,0 +1,22 @@
+package Lex;
+
+public class Lex_1
+{
+
+
+
+   // for (i= 0; i< 10; i++)
+   // {
+     //   if (i!=3&&9);
+    //    {
+       //     System.out.println(i+"Я узучаю программирование");
+    //    }
+  //      else
+       //     {
+
+      //      System.out.println(+1"Меня интересует ЗП");
+ //   }
+//}
+
+
+}
