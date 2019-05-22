ad5e79e5eeada1d54c3ae7dd12d3c19358a675ff
pin2piva <happyowner88@gmail.com>
2019-05-22 22:56:35 +0300

    Lesson4 TaskA1

--- src/gabrusev/lesson4/lesson4_a.java
+++ src/gabrusev/lesson4/lesson4_a.java
@@ -2,15 +2,15 @@ package gabrusev.lesson4;
 
 public class lesson4_a {
     public static void main(String[] args) {
-        String a = "Я изучаю программирование";
-        String b = "Меня интересует высокая зарплата";
+        String c = "Я изучаю программирование";
+        String d = "Меня интересует высокая зарплата";
         int i;
         for (i = 0; i < 10; i++) {
             if (i != 2 && i != 8) {
-                System.out.println(a);
+                System.out.println(c);
             }
             else {
-                System.out.println(b);
+                System.out.println(d);
             }
         }
     }
