f62f4574250273bd9ea75778ad2dfd1800fae496
dmbelov <dmbelov777@gmail.com>
2019-05-25 09:31:26 +0300

    Lesson4 Task C2 changed array initialization

---
+++ src/belov/Lesson4/Lesson4TaskC3.java
@@ -0,0 +1,11 @@
+package belov.Lesson4;
+
+public class Lesson4TaskC3 {
+    public static void main(String[] args) {
+
+        int x  = 3;
+        String y = Integer.toString(x);
+        System.out.print(x + " " + y);
+
+    }
+}
