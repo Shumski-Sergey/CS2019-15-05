1682d36caf049c9858aa3645a8fb7d088b33b1c6
NatalliaRmn <natalliaremeniako@gmail.com>
2019-05-28 11:22:17 -0800

    5/19/2019 by Remeniaka

---
+++ src/remeniaka/Lesson5/service/starter.java
@@ -0,0 +1,21 @@
+package remeniaka.Lesson5.service;
+
+import sshumsky.lesson6.service.impl.DogServiceImpl;
+
+public class starter {
+
+    public static void main (String[] args) {
+
+
+        Dog first=new Dog (1,"Рэсси");
+
+        Dog second=new Dog(6,"Арчи");
+
+
+        System.out.print("Кличкa"+ first.getName());
+    }
+
+
+
+
+}
